6675c64aa03122f4b86a0a239cdc57f500e3ac91
unknown <edupv10@hotmail.com>
2026-05-10 00:25:22 -0700

    Add GTM

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-P4BM3DGP'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
